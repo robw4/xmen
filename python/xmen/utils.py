a692bc7ea31af51890c828ddc836456b1f7776e3
hazelwood-farm <robw@robots.ox.ac.uk>
2019-09-19 11:24:51 +0100

    Added copywrite information

--- python/xmen/utils.py
+++ python/xmen/utils.py
@@ -1,5 +1,23 @@
 """A module containing several utilitity functions, classes and Meta Classes used by the ExperimentManager
 and the Experiment classes."""
+#  Copyright (C) 2019  Robert J Weston, Oxford Robotics Institute
+#
+#  xmen
+#  email:   robw@robots.ox.ac.uk
+#  github: https://github.com/robw4/xmen/
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import inspect
 import git
