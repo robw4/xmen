444ed30de5c6464fb3f0138167246876ad1f313c
hazelwood-farm <robw@robots.ox.ac.uk>
2020-02-14 17:31:19 +0000

    Added setup.py

---
+++ python/xmen/setup.py
@@ -0,0 +1,58 @@
+#  Copyright (C) 2019  Robert J Weston, Oxford Robotics Institute
+#
+#  xmen
+#  email:   robw@robots.ox.ac.uk
+#  github: https://github.com/robw4/xmen/
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#  xmen
+#  email:   robw@robots.ox.ac.uk
+#  github: https://github.com/robw4/xmen/
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#  xmen
+#  email:   robw@robots.ox.ac.uk
+#  github: https://github.com/robw4/xmen/
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#   along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+setup(
+    name='xmen',
+    version='0.1',
+    description='xmen experiment management libraries for fast reproducible experimentation',
+    author='Rob Weston',
+    author_email='robw@robots.ox.ac.uk',
+    packages=[''])
