1a8f3a3541c5ea432ecbdf8f632abb26b4a89010
hazelwood-farm <robw@robots.ox.ac.uk>
2020-11-17 12:59:10 +0000

    Bumped version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 
 setup(
     name='xmen',
-    version='0.2.0',
+    version='0.2.1',
     description='Experiment management libraries for fast, reproducible, remote and local experimentation',
     author='Rob Weston',
     author_email='robw@robots.ox.ac.uk',
